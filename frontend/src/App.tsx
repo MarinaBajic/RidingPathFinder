8ed25488e40ee5988e45add01996237bbfc8e154
MarinaBajic <marinabajic10@gmail.com>
2025-04-05 23:02:23 +0200

    FE: update styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Hero from './components/Hero';
 import Map from './components/Map';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-		<div className="App">
-			<h1>Leaflet Map</h1>
+		<div className="content-grid my-16">
+			<Hero />
 			<Map />
 		</div>
   );
