d6deb92963f804d1da67567b586a18ee2c835f3b
MarinaBajic <marinabajic10@gmail.com>
2025-04-27 19:32:28 +0200

    FE: refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,12 @@ import './App.css'
 import Hero from './components/hero/Hero';
 import 'leaflet/dist/leaflet.css';
 import MapSection from './components/mapSection/MapSection';
-import { MapProvider } from './context/MapContext';
 
 function App() {
 	return (
 		<div className="content-grid my-16">
 			<Hero />
-			<MapProvider>
-				<MapSection />
-			</MapProvider>
+			<MapSection />
 		</div>
 	);
 }
