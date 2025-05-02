4224c650c07f4b61193ebd1a262dee5d28a32b7b
MarinaBajic <marinabajic10@gmail.com>
2025-05-03 00:30:09 +0200

    FE: deleted POI

--- frontend/src/services/poiService.ts
+++
@@ -1,6 +0,0 @@
-import { API_BASE } from "./mapService";
-
-export const fetchPois = async (bounds: L.LatLngBounds, zoom: number) => {
-	const res = await fetch(`${API_BASE}/pois?minLng=${bounds.getWest()}&minLat=${bounds.getSouth()}&maxLng=${bounds.getEast()}&maxLat=${bounds.getNorth()}&zoom=${zoom}`);
-	return await res.json();
-};
--- frontend/src/types/Poi.ts
+++
@@ -1,7 +0,0 @@
-export interface Poi {
-    id: number;
-    name: string;
-    fclass: string;
-    latitude: number;
-    longitude: number;
-}
