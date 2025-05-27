d43de3ab9593ad3abe9811e19111d0421878be53
MarinaBajic <marinabajic10@gmail.com>
2025-05-27 16:21:21 +0200

    FE: added switch for OSM and raster layer

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -13,9 +13,22 @@ const Map = ({ mapRef, onInit }: MapProps) => {
 		if (mapContainer.current && !mapRef.current) {
 			mapRef.current = L.map(mapContainer.current).setView([45.2671, 19.8335], 13);
 
-			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				attribution: '© OpenStreetMap contributors',
 			}).addTo(mapRef.current);
+
+			const raster = L.tileLayer.wms('http://localhost:8080/geoserver/ne/wms', {
+				layers: 'ne:world',
+				format: 'image/png',
+				transparent: true
+			});
+
+			const baseLayers = {
+				'OpenStreetMap': osm,
+				'Raster Layer': raster,
+			};
+
+			L.control.layers(baseLayers).addTo(mapRef.current);
 
 			onInit?.();
 		}
