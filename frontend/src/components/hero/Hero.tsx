ccbe976e2907913ec2e927887616e4182a1deba9
MarinaBajic <marinabajic10@gmail.com>
2025-05-30 16:20:46 +0200

    FE: responsiveness

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -3,8 +3,8 @@ import Button from "../button/Button";
 const Hero = () => {
 
     return (
-        <section className="flex items-center justify-between scroll-mt-32">
-            <div className="space-y-12">
+        <section id="home" className="flex flex-col gap-12 md:flex-row md:gap-0 items-center justify-between scroll-mt-32">
+            <div className="space-y-12 flex flex-col items-center md:items-start text-center md:text-left">
                 <div>
                     <h1 className="text-7xl">Riding<br /> Path Finder</h1>
                     <p>Easily find Rides and Trails in Serbia!</p>
@@ -15,9 +15,9 @@ const Hero = () => {
                 </div>
             </div>
             <div className="flex gap-6">
-                <div className="w-96 aspect-[3/4] bg-cover bg-right" style={{ backgroundImage: "url('src/assets/hero.jpg')" }}></div>
+                <div className="w-48 md:w-96 aspect-[3/4] bg-cover bg-right" style={{ backgroundImage: "url('src/assets/hero.jpg')" }}></div>
                 <div className="flex flex-col items-center justify-center gap-3">
-                    <div className="w-48 aspect-[3/4] bg-cover" style={{ backgroundImage: "url('src/assets/hero2.jpg')" }}></div>
+                    <div className="w-36 md:w-48 aspect-[3/4] bg-cover" style={{ backgroundImage: "url('src/assets/hero2.jpg')" }}></div>
                     <Button href="#" hierarchy="tertiary">Bla bla</Button>
                 </div>
 
