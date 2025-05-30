8aad50b4e80b406503a1fa55fa32c1a4cfca1a5d
MarinaBajic <marinabajic10@gmail.com>
2025-05-30 15:56:47 +0200

    FE: updated instructions

--- frontend/src/components/instructions/Instructions.tsx
+++ frontend/src/components/instructions/Instructions.tsx
@@ -5,8 +5,11 @@ const Instructions = () => {
                 <h3 className="text-lg font-bold mb-4">Instructions</h3>
                 <p>On the map below:</p>
                 <ul className="list-disc list-inside mb-4">
-                    <li>Click on a marker to see details of waypoint.</li>
+                    <li>Click on a marker to see details of a waypoint.</li>
                     <li>Adjust the radius to highlight other waypoints within that range.</li>
+                    <li>In the right top corner click to change base map and add an overlay.</li>
+                    <li>Show all paths, click to show details and nearby waypoints.</li>
+                    <li>Add new waypoint and delete an existing waypoint.</li>
                 </ul>
             </div>
         </div>
