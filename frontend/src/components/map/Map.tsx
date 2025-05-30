1b9102f71cf2e09a98976b69bb8259c950005b2a
MarinaBajic <marinabajic10@gmail.com>
2025-05-30 15:56:39 +0200

    FE: map overlays and using parameters for WFS

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -8,6 +8,27 @@ interface MapProps {
 
 const Map = ({ mapRef, onInit }: MapProps) => {
 	const mapContainer = useRef(null);
+	let highlightLayer: L.Layer | null = null;
+
+	const highlightFeature = (url: string, propertyName: string) => {
+		if (highlightLayer && mapRef.current) {
+			mapRef.current.removeLayer(highlightLayer);
+			highlightLayer = null;
+		}
+
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				highlightLayer = L.geoJSON(data, {
+					style: { color: 'red', weight: 3 },
+					onEachFeature: (feature, layer) => {
+						layer.bindPopup(`${feature.properties[propertyName] || 'No name'}`);
+					}
+				}).addTo(mapRef.current!);
+
+				mapRef.current?.fitBounds((highlightLayer as L.GeoJSON).getBounds());
+			});
+	};
 
 	useEffect(() => {
 		if (mapContainer.current && !mapRef.current) {
@@ -23,12 +44,53 @@ const Map = ({ mapRef, onInit }: MapProps) => {
 				transparent: true
 			});
 
+			const opstine = L.geoJSON(null, {
+				onEachFeature: (feature, layer) => {
+					layer.bindPopup(feature.properties.NAME_2 || 'No name');
+					layer.on('click', () => {
+						const name = feature.properties.NAME_2;
+						const url = `http://localhost:8080/geoserver/rpf/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=rpf:opstine&outputFormat=application/json&cql_filter=NAME_2='${name}'`;
+						highlightFeature(url, 'NAME_2');
+					});
+				}
+			});
+			fetch("http://localhost:8080/geoserver/rpf/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=rpf%3Aopstine&outputFormat=application%2Fjson")
+				.then(res => res.json())
+				.then(data => opstine.addData(data));
+
+			const okruzi = L.geoJSON(null, {
+				onEachFeature: (feature, layer) => {
+					layer.bindPopup(feature.properties.NAME_1 || 'No name');
+					layer.on('click', () => {
+						const name = feature.properties.NAME_1;
+						const url = `http://localhost:8080/geoserver/rpf/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=rpf:okruzi&outputFormat=application/json&cql_filter=NAME_1='${name}'`;
+						highlightFeature(url, 'NAME_1');
+					});
+				}
+			});
+			fetch("http://localhost:8080/geoserver/rpf/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=rpf%3Aokruzi&outputFormat=application%2Fjson")
+				.then(res => res.json())
+				.then(data => okruzi.addData(data));
+
+
 			const baseLayers = {
 				'OpenStreetMap': osm,
-				'Raster Layer': raster,
+				'NaturalEarth Map': raster,
 			};
 
-			L.control.layers(baseLayers).addTo(mapRef.current);
+			const overlays = {
+				"Show Districts": okruzi,
+				"Show Municipalities": opstine
+			};
+
+			L.control.layers(baseLayers, overlays).addTo(mapRef.current);
+
+			mapRef.current.on('overlayremove', () => {
+				if (highlightLayer) {
+					mapRef.current?.removeLayer(highlightLayer);
+					highlightLayer = null;
+				}
+			});
 
 			onInit?.();
 		}
